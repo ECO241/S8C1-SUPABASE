361dbc85bfd3703cb51b69d5c6064ea6dfc92339
josemoncada87 <josemoncada87@hotmail.com>
2024-03-18 15:05:16 -0500

    Refactor Supabase connection and add user creation

--- OlderSamples/index_sample.js
+++ OlderSamples/index_sample.js
@@ -1,19 +1,27 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://puplqkhpqxjgsfwuqikb.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cGxxa2hwcXhqZ3Nmd3VxaWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0Njg3MzEsImV4cCI6MjAyNjA0NDczMX0.5FKItdFAo4XZoqufgHi_FQcgsmwddD7lZ-xjVMblXu0"
-const supabase = createClient(supabaseUrl, supabaseKey)
-//console.log(supabase)
-/*
+// database connection
+import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'
+
+const supabaseUrl = process.env.SUPABASE_TEAM;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error("Supabase URL or key is missing. Make sure to set SUPABASE_TEAM and SUPABASE_KEY environment variables.");
+    process.exit(1);
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 // Get all users
 const { data, error } = await supabase
   .from('users')
   .select();
 console.log(data, error)
-*/
 
 // Create a new user
-const { data, error } = await supabase
+const { dataA, errorA } = await supabase
   .from('users')
   .insert({ name: 'Andrés', mail: 'mm@icesi.edu.co'})
-console.log(data, error)
+console.log(dataA, errorA)
